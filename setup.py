bae6dd8af1790851d2cb17cfb2a6cf723bc8188e
Manazael Jora <manazaeljora@gmail.com>
2025-12-12 23:12:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-# Leitura segura do README
+# Leitura segura do README para evitar erro de metadata
 long_description = "Ferramenta para planejamento de misturas Simplex-Lattice."
 if os.path.exists("README.md"):
     with open("README.md", "r", encoding="utf-8") as f:
