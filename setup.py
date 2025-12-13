439a8a425f6a9652ae3a874899a3f0e482d4df18
Manazael Jora <manazaeljora@gmail.com>
2025-12-12 22:49:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import os
 from setuptools import setup, find_packages
+
+# Leitura segura do README
+long_description = "Ferramenta para planejamento de misturas Simplex-Lattice."
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
 
 setup(
     name="simplex_lattice_design",
     version="69.6",
     author="Manazael Zuliani Jora",
     description="Ferramenta para planejamento de misturas Simplex-Lattice.",
-    long_description="A comprehensive Python tool designed for formulation scientists to generate Simplex-Lattice mixture designs.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ManasJora/simplex_lattice_design",
     packages=find_packages(),
